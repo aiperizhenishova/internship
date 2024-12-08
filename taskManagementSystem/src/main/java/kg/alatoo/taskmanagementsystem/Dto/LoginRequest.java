dcc3e5c9983bf5da6495b2eb10a224c1ac54c877
Aiperi <122115154+aiperizhenishova@users.noreply.github.com>
2024-12-08 19:01:55 +0600

    Delete taskManagementSystem directory

--- taskManagementSystem/src/main/java/kg/alatoo/taskmanagementsystem/Dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package kg.alatoo.taskmanagementsystem.Dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {        //класс, который принимает данные для входа (email и password):
-    private String email;
-    private String password;
-}
